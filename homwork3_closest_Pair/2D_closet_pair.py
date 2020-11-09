d2278683a27ff8589945d44f8bb5c94a82b27fab
huangJC0429 <1415139229@qq.com>
2020-11-09 22:11:23 +0800

    assignment3

---
+++ homwork3_closest_Pair/2D_closet_pair.py
@@ -0,0 +1,76 @@
+import math
+import matplotlib.pyplot as plt
+
+class CP():
+    def init_sort(self, P, n):
+        X = list(P)
+        Y = list(P)
+        #  根据横坐标排序
+        X.sort()
+        Y = CP().sort_y(Y)
+        return X, Y, n
+
+
+    def sort_y(self, Y):
+        return sorted(Y, key=lambda last: last[-1])
+
+
+    def closest_pair(self, X, Y, n):
+        if n <= 3:
+            return CP().brute_force(X, n)
+        mid = n // 2
+        Y_left = []
+        Y_right = []
+        for p in Y:
+            if p in X[:mid]:
+                Y_left.append(p)
+            else:
+                Y_right.append(p)
+        dis_left = CP().closest_pair(X[:mid], Y_left, mid)
+        dis_right = CP().closest_pair(X[mid:], Y_right, n - mid)
+        min_dis = min(dis_left, dis_right)
+        strip = []
+        for (x, y) in Y:
+            if abs(x - X[mid][0]) < min_dis:
+                strip.append((x, y))
+        #  检查最近的6个
+        return min(min_dis, CP().strip_closest(strip, min_dis))
+
+
+    def brute_force(self, X, n):
+        min_d = CP().compute_distance(X[0], X[1])
+        for i, (x, y) in enumerate(X):
+            for j in range(i + 1, n):
+                if CP().compute_distance(X[i], X[j]) < min_d:
+                    min_d = CP().compute_distance(X[i], X[j])
+        return min_d
+
+
+    def compute_distance(self, a, b):
+        return math.sqrt(math.pow((a[0] - b[0]), 2) + math.pow((a[1] - b[1]), 2))
+
+
+    def strip_closest(self, strip, d):
+        min_d = d
+        for i, (x, y) in enumerate(strip):
+            for j in range(i + 1, 6):
+                if i + j < len(strip):
+                    temdis = CP().compute_distance(strip[i], strip[j])
+                    if temdis < min_d:
+                        min_d = temdis
+        return min_d
+
+
+if __name__ == "__main__":
+    points = [(8, 48), (10, 3), (14, 27), (9, 15),
+              (12, 13), (8, 19), (12, 30), (25, 30),
+              (9, 12), (13, 10), (9, 4), (15, 6),
+              (22, 32), (5, 32), (23, 9), (19, 25),
+              (14, 1), (11, 25), (26, 26), (12, 19),
+              (18, 9), (27, 13), (32, 13), (15, 16)]
+
+    for i in points:
+        plt.scatter(i[0], i[1])
+    plt.show()
+    X, Y, n = CP().init_sort(points, len(points))
+    print(CP().closest_pair(X, Y, n))
