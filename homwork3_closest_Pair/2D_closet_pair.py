108287adaa757da510b9c7b68d1b0ac021a11cc8
huangJC0429 <1415139229@qq.com>
2020-11-10 19:40:40 +0800

    assignment4

--- homwork3_closest_Pair/2D_closet_pair.py
+++ homwork3_closest_Pair/2D_closet_pair.py
@@ -1,19 +1,15 @@
 import math
 import matplotlib.pyplot as plt
 
-class CP():
+
+class CP:
     def init_sort(self, P, n):
         X = list(P)
         Y = list(P)
         #  根据横坐标排序
         X.sort()
-        Y = CP().sort_y(Y)
+        Y = sorted(Y, key=lambda last: last[-1])
         return X, Y, n
-
-
-    def sort_y(self, Y):
-        return sorted(Y, key=lambda last: last[-1])
-
 
     def closest_pair(self, X, Y, n):
         if n <= 3:
@@ -34,8 +30,14 @@ class CP():
             if abs(x - X[mid][0]) < min_dis:
                 strip.append((x, y))
         #  检查最近的6个
-        return min(min_dis, CP().strip_closest(strip, min_dis))
-
+        min_d = min_dis
+        for i, (x, y) in enumerate(strip):
+            for j in range(i + 1, 6):
+                if i + j < len(strip):
+                    temdis = CP().compute_distance(strip[i], strip[j])
+                    if temdis < min_d:
+                        min_d = temdis
+        return min(min_dis, min_d)
 
     def brute_force(self, X, n):
         min_d = CP().compute_distance(X[0], X[1])
@@ -45,32 +47,20 @@ class CP():
                     min_d = CP().compute_distance(X[i], X[j])
         return min_d
 
-
     def compute_distance(self, a, b):
         return math.sqrt(math.pow((a[0] - b[0]), 2) + math.pow((a[1] - b[1]), 2))
 
 
-    def strip_closest(self, strip, d):
-        min_d = d
-        for i, (x, y) in enumerate(strip):
-            for j in range(i + 1, 6):
-                if i + j < len(strip):
-                    temdis = CP().compute_distance(strip[i], strip[j])
-                    if temdis < min_d:
-                        min_d = temdis
-        return min_d
-
-
 if __name__ == "__main__":
-    points = [(8, 48), (10, 3), (14, 27), (9, 15),
+    data = [(8, 48), (10, 3), (14, 27), (9, 25),
               (12, 13), (8, 19), (12, 30), (25, 30),
-              (9, 12), (13, 10), (9, 4), (15, 6),
+              (9, 12), (13, 10), (9, 9), (15, 6),
               (22, 32), (5, 32), (23, 9), (19, 25),
               (14, 1), (11, 25), (26, 26), (12, 19),
               (18, 9), (27, 13), (32, 13), (15, 16)]
 
-    for i in points:
+    for i in data:
         plt.scatter(i[0], i[1])
     plt.show()
-    X, Y, n = CP().init_sort(points, len(points))
-    print(CP().closest_pair(X, Y, n))
+    X, Y, n = CP().init_sort(data, len(data))
+    print("最近点对距离为:", CP().closest_pair(X, Y, n))
---
+++ homwork4_Knapsack/DP_Knapsack.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+Items = [(1, 1, 1), (2, 6, 2), (3, 18, 5), (4, 22, 6), (5, 28, 7)]
+
+if __name__ == '__main__':
+    M = np.zeros((6, 12))
+    for w in range(12):
+        M[0, w] = 0
+    for i in range(1, 6):
+        for w in range(1, 12):
+            wi = Items[i - 1][2]
+            if wi > w:
+                M[i, w] = M[i - 1, w]
+            else:
+                M[i, w] = max(M[i - 1, w], Items[i - 1][1] + M[i - 1, w - wi])
+    print(M[5, 11])
