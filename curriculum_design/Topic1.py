a6394c1969ee4689f30adff6af4225fbf6e7052c
huangJC0429 <1415139229@qq.com>
2020-11-14 22:02:20 +0800

    curriculum_design_topic1

---
+++ curriculum_design/Topic1.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+R = np.array([["成都", "巴西利亚"],
+              ["普利茅斯", "乌斯怀亚"],
+              ["巴西利亚", "普利茅斯"],
+              ["大溪地", "乌斯怀亚"]])
+
+print(list(filter(lambda x: x not in list(set(R[:, 0])), list(set(list(set(R[:, 1])) + (list(set(R[:, 0]))))))))
