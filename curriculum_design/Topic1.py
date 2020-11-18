d3bbbdb6a0e3f180d252a8231a734ffee9c4e765
huangJC0429 <1415139229@qq.com>
2020-11-18 10:59:28 +0800

    update Topic1

--- curriculum_design/Topic1.py
+++ curriculum_design/Topic1.py
@@ -5,4 +5,5 @@ R = np.array([["成都", "巴西利亚"],
               ["巴西利亚", "普利茅斯"],
               ["大溪地", "乌斯怀亚"]])
 
-print(list(filter(lambda x: x not in list(set(R[:, 0])), list(set(list(set(R[:, 1])) + (list(set(R[:, 0]))))))))
+# print(list(filter(lambda x: x not in list(set(R[:, 0])), list(set(list(set(R[:, 1])) + (list(set(R[:, 0]))))))))
+print(list(set(list(set(R[:, 1]))).difference(set(list(set(R[:, 0]))))))
